533474b1e2fba1be9d97cea7cf896ab986dfcbd3
pratik0199 <sonawanepratik1999@gmail.com>
2024-05-13 10:29:58 +0530

    Update site.py

--- site.py
+++ site.py
@@ -7,7 +7,7 @@ import seaborn as sns
 st.header('Compressor Health Monitoring')
 #st.subheader('This is our website')
 
-excel_file = pd.ExcelFile(r'https://github.com/pratik0199/comp_dashboard/blob/main/GERC_A_PARA_GUI_rev4.xlsm')
+excel_file = pd.ExcelFile(r'https://github.com/pratik0199/comp_dashboard/blob/main/GERC_A_PARA_GUI_rev4.xlsm',engine='openpyxl')
 dict_stg = {'Stage 1': {'tab1': [7, 368, 'table1_df'], 'tab2': [372, 733, 'table2_df'], 'tab3': [745, 1106, 'table3_df'], 'tab4': [1110, 1471,'table4_df']},
             'Stage 2': {'tab1': [7, 368, 'table1_df'], 'tab2': [372, 733, 'table2_df'], 'tab3': [745, 1106, 'table3_df'], 'tab4': [1110, 1471,'table4_df']}
            }
@@ -135,7 +135,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 # Load data from Excel file
-database_df = pd.read_excel(r'https://github.com/pratik0199/comp_dashboard/blob/main/GERC_A_PARA_GUI_rev4.xlsmm', sheet_name='database')
+database_df = pd.read_excel(r'https://github.com/pratik0199/comp_dashboard/blob/main/GERC_A_PARA_GUI_rev4.xlsmm',engine='openpyxl', sheet_name='database')
 #st.write(database_df[' Stg I Flow factor'].iloc[1:26]).head()
 
 # Function to update the plot data for Stage I
